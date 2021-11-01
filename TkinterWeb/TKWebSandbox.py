64d1aa7f613a6d5502c332176170f8c459179080
lol5230 <lol5230@psu.edu>
2021-11-01 18:19:42 -0400

    Created a tkinterweb sandbox to play with

---
+++ TkinterWeb/TKWebSandbox.py
@@ -0,0 +1,16 @@
+from tkinterweb import HtmlFrame
+import tkinter as tk
+import sys
+
+root = tk.Tk()
+root.geometry("1000x1000")
+
+htmlGroup = tk.Frame(root, width="500", height="500")
+htmlFrame = HtmlFrame(htmlGroup)
+
+# htmlFrame.load_website("http://tkhtml.tcl.tk/tkhtml.html")
+htmlFrame.load_html("C:/Users/leite/Desktop/Repos/TKPythonSandbox/output.html")
+
+htmlFrame.pack(fill="both", expand=True)
+htmlGroup.pack(side=tk.LEFT)
+root.mainloop()
